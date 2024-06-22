67d7683af4d34a894d1f1986b83c0b0e097238e6
bit-web24 <bitweb24@gmail.com>
2024-06-22 16:19:35 +0530

    impl sidenav sections

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home_view
+from .views import dashboard, students, teachers, classes, courses, exams
 
 urlpatterns = [
-    path('', home_view, name='dashboard'),
+    path('', dashboard, name='dashboard'),
+    path('students', students, name='students'),
+    path('teachers', teachers, name='teachers'),
+    path('classes', classes, name='classes'),
+    path('courses', courses, name='courses'),
+    path('exams', exams, name='exams'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
-def home_view(request):
-    return render(request, 'admin_panel/dashboard.html')
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def students(request):
+    return render(request, 'students/dashboard.html')
+
+def teachers(request):
+    return render(request, 'teachers/dashboard.html')
+
+def courses(request):
+    return render(request, 'courses/dashboard.html')
+
+def classes(request):
+    return render(request, 'classes/dashboard.html')
+
+def exams(request):
+    return render(request, 'exams/dashboard.html')
