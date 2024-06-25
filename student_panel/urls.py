051d4d1b984ca3c5a937a47b5020c0754e4d674a
bit-web24 <bitweb24@gmail.com>
2024-06-25 16:37:22 +0530

    show exams assigned to class

--- student_panel/urls.py
+++ student_panel/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:student_id>/', views.student_dashboard, name='student_dashboard'),
     path('<int:student_id>/exams/', views.student_exams, name='student_exams'),
+    path('<int:student_id>/exams/<int:exam_id>', views.student_exam_detail, name='student_exam_detail'),
     # path('marks', views.student_marks, name='marks'),
 ]
--- student_panel/views.py
+++ student_panel/views.py
@@ -14,3 +14,9 @@ def student_exams(request, student_id):
     _class = get_object_or_404(Class, pk=student.class_id.pk)
     exams = _class.exams.all()
     return render(request, 'student_exams/dashboard.html', {'student': student, 'exams': exams})
+
+def student_exam_detail(request, student_id, exam_id):
+    student = get_object_or_404(Student, pk=student_id)
+    _class = get_object_or_404(Class, pk=student.class_id.pk)
+    exam = get_object_or_404(_class.exams, pk=exam_id)
+    return render(request, 'student_exams/exam_detail.html', {'student': student, 'class': _class, 'exam': exam})
