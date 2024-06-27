f758feb1626d108a8e06f73edd18f7766c5f8eaf
bit-web24 <bitweb24@gmail.com>
2024-06-27 19:17:49 +0530

    fix face detection

--- auth0/views.py
+++ auth0/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from admin_panel.models import Student, Teacher
 import base64
-import numpy as np
-import cv2
+import pathlib
 import os
 from deepface import DeepFace
 
@@ -84,17 +83,14 @@ def capture_face(request):
 
     return render(request, 'capture_face.html')
 
-def byte_to_png(photo, studentId, name):
-    directory = f"captured/{studentId}"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    # Assuming photo is already bytes-like, no need to decode again
-    nparr = np.frombuffer(photo, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    face_filename = os.path.join(directory, f"{name}.png")
-    cv2.imwrite(face_filename, img_np)
+def byte_to_png(data, name):
+    captured =  pathlib.Path('captured')
+    if not captured.exists():
+        captured.mkdir()
+    face_filename = captured.joinpath(f'{name}.png')
+    face_filename = str(face_filename)
+    with open(face_filename, 'wb') as f:
+        f.write(data)
     return face_filename
 
 def verify_face(request):
@@ -110,11 +106,14 @@ def verify_face(request):
                 img_data = base64.b64decode(face_image.split(',')[1])
                 stored_face_data = student.face_image
 
-                path1 = byte_to_png(img_data, student.pk, 'login_image')
-                path2 = byte_to_png(stored_face_data, student.pk, 'stored_image')
+                path1 = byte_to_png(img_data, 'current_image')
+                path2 = byte_to_png(stored_face_data, 'stored_image')
 
                 res = DeepFace.verify(img1_path=path1, img2_path=path2)
-
+                
+                import shutil
+                shutil.rmtree(pathlib.Path('captured'))
+                
                 if res['verified']:
                     return redirect('student_dashboard', student_id=student_id)
                 else:
